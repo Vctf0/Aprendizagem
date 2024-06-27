c171d227ea7959741c66e368e0fa133fc1550816
Vctf0 <jvictor.ferreia@gmail.com>
2024-06-27 10:44:31 -0300

    Aulas js

---
+++ app.js
@@ -0,0 +1,39 @@
+// let lista = [["vermelho","azul","amarelo","preto"],"Kombi","Willys","Caravan","Opala","Belina","Corcel","Variant","MCQueen","Fusquinha","Monza","Vectra"];
+
+// console.log(lista[0][1])
+
+// let exemplo = [1,3,["Teste","Desse",[true,2,"Undefined"]],[5,["Bulldog","Shitzu",[12,42,60,["Chevette","porsche"]]]],["Pera","Uva",[2,4,6,3]]]
+
+// console.log(exemplo[3][1][2][3][1])
+
+// let exemplo = [1,3,["Teste","Desse",[true,2,"Undefined"]],[5,["Bulldog","Shitzu",[12,42,60,["Chevette","porsche"]]]],["Pera","Uva",[2,4,6,3]]]
+
+// console.log(exemplo [4][2][2])
+
+// let exemplo = [1,3,["Teste","Desse",[true,2,"Undefined"]],[5,["Bulldog","Shitzu",[12,42,60,["Chevette","porsche"]]]],["Pera","Uva",[2,4,6,3]]]
+
+// console.log(exemplo [2][2][2])
+
+// let exemplo = [1,3,["Teste","Desse",[true,2,"Undefined"]],[5,["Bulldog","Shitzu",[12,42,60,["Chevette","porsche"]]]],["Pera","Uva",[2,4,6,3]]]
+
+// console.log(exemplo [3][1][1])
+
+// let obj = {
+//     id: 1,
+//     nome: "Victor",
+//     cpf: "00100200304",
+//     email: "vitu@gmail.com"
+// }
+// let texto = `O usuário de nome ${obj.nome} e de email ${obj.email} está com fome`
+// console.log(texto)
+
+let carro = {
+    modelo:"Mcqueen",
+    ano:"2000",
+    cor:"vermelho",
+    chassi:"jn27062024dushud"
+}
+// let texto= `O carro de modelo ${carro.modelo} e de ano ${carro.ano} está na oficina há 20 dias`
+// console.log (texto)
+let texto= `O carro de chassi ${carro.chassi} está no pátio desde o ano ${carro.ano}`
+console.log (texto)
